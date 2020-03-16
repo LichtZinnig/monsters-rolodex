cbfe8e62286465c2602aa8a5fd66c1fb912a2d16
LichtZinnig <lucas@lichtzinnig.nl>
2020-03-16 20:37:57 +0100

    Updates

---
+++ precache-manifest.88a204ff3d9b8b78b6dd169b3993b1b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110009e83e4f6a4079ce2d2ad699c444",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "17412448c4a696c43e91",
+    "url": "/monsters-rolodex/static/css/main.e9b3e13c.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17412448c4a696c43e91",
+    "url": "/monsters-rolodex/static/js/main.ac696cc2.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
